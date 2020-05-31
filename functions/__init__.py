81f51cca974ae62a5ed0fdcc7f075fbf551011f2
Davi Honorato <davii.silva64@gmail.com>
2020-05-31 12:44:54 -0300

    Criando a lógica inicial

--- functions/__init__.py
+++ functions/__init__.py
@@ -7,6 +7,20 @@ def menu():  # Mostra o menu com todas as opções de interação.
     print('-' * 30)
 
 
+'''def atualizar():  # verifica se o arquivo existe (caso não encontre, cria o arquivo)
+    try:
+        arq = open('jogos.txt', 'rt')
+    except:
+        try:
+            arq = open('jogos.txt', 'wt+')
+        except:
+            print('Ocorreu um erro ao criar o arquivo.')
+        else:
+            arq.close()
+    else:
+        arq.close()
+
+
 def leiaEnt(txt):  # Lê uma entrada e verifica se ela está dentro do parâmetro.
     escolha = ['1', '2', '3', '4', '0']
     try:
@@ -20,51 +34,4 @@ def leiaEnt(txt):  # Lê uma entrada e verifica se ela está dentro do parâmetr
     except:
         print('Ocorreu um erro.')
     else:
-        return resp
-
-
-def adicionar():  # adiciona um jogo na lista
-    print('-' * 30)
-    print(f'{"OPÇÃO 1":^30}')
-    print('-' * 30)
-
-
-def deletar():  # deleta um jogo da lista (caso não tenha nenhum, irá aparecer uma mensagem)
-    print('-' * 30)
-    print(f'{"OPÇÃO 2":^30}')
-    print('-' * 30)
-
-
-def pesquisar():  # faz uma pesquisa e mostra as informações existentes (caso não encontre, mostra uma msg)
-    print('-' * 30)
-    print(f'{"OPÇÃO 3":^30}')
-    print('-' * 30)
-
-
-def ver():  # exibe a lista completa (caso não tenha nenhuma, irá aparecer uma mensagem)
-    print('-' * 30)
-    print(f'{"OPÇÃO 4":^30}')
-    print('-' * 30)
-
-
-def atualizar():  # verifica se o arquivo existe (caso não encontre, cria o arquivo)
-    try:
-        arq = open('jogos.txt', 'rt')
-    except:
-        try:
-            arq = open('jogos.txt', 'wt+')
-        except:
-            print('Ocorreu um erro ao criar o arquivo.')
-        else:
-            arq.close()
-    else:
-        arq.close()
-
-    '''while True:  # atualizar a lista por ondem alfabética.
-        if vazio:
-            pass
-        else:
-            with open('jogos.txt', 'rt') as r:
-                p = r.readlines()
-            for c in p:'''
-
+        return resp'''
--- main.py
+++ main.py
@@ -3,16 +3,4 @@ from time import sleep
 
 while True:
     menu()
-    atualizar()
-    resp = leiaEnt('SUA ESCOLHA: ')
-    if resp == 1:
-        adicionar()
-    elif resp == 2:
-        deletar()
-    elif resp == 3:
-        pesquisar()
-    elif resp == 4:
-        ver()
-    else:
-        break
     sleep(1)
