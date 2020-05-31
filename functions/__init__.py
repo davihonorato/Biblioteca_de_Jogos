f681bbb1fccdac2d334d0c34804321f1f15470ed
Davi Honorato <davii.silva64@gmail.com>
2020-05-31 14:43:01 -0300

    Criação da lógica principal

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,10 +1,49 @@
-def menu():  # Mostra o menu com todas as opções de interação.
+def interface():  # Mostra o menu com todas as opções de interação.
     print('-' * 30)
     print(f'{"LISTA DE JOGOS ZERADOS":^30}')
     print('-' * 30)
     print('ESCOLHA UMA OPÇÃO ABAIXO: ')
     print('[ 1 ] ADICIONAR \n[ 2 ] DELETAR \n[ 3 ] PESQUISAR \n[ 4 ] VER LISTA COMPLETA \n[ 0 ] SAIR')
     print('-' * 30)
+
+
+class Menu:
+    def __init__(self):
+        self.__lista = dict()
+
+    @property
+    def lista(self):
+        return self.__lista
+
+    def adicionar(self, nome, data, tempo):
+        if 'jogos' not in self.__lista:
+            self.__lista['jogos'] = {nome: {'data': data, 'tempo': tempo}}
+        elif nome in self.__lista['jogos']:
+            print('O jogo já está na lista.')
+        else:
+            self.__lista['jogos'].update({nome: {'data': data, 'tempo': tempo}})
+
+    def deletar(self, nome):
+        if 'jogos' not in self.__lista:
+            print('A lista está vazia.')
+        elif nome not in self.__lista['jogos']:
+            print('O jogo não existe na lista.')
+        else:
+            del self.__lista['jogos'][nome]
+
+    def pesquisar(self, nome):
+        if nome not in self.__lista['jogos']:
+            print('O jogo não existe na lista.')
+        else:
+            print(nome)
+            for i in self.__lista['jogos'][nome].values():
+                print(i)
+
+    def listar(self):
+        for nome in self.__lista['jogos']:
+            print(nome)
+            for i in self.__lista['jogos'][nome].values():
+                print(i)
 
 
 '''def atualizar():  # verifica se o arquivo existe (caso não encontre, cria o arquivo)
@@ -18,7 +57,7 @@ def menu():  # Mostra o menu com todas as opções de interação.
         else:
             arq.close()
     else:
-        arq.close()
+        arq.close()'''
 
 
 def leiaEnt(txt):  # Lê uma entrada e verifica se ela está dentro do parâmetro.
@@ -34,4 +73,4 @@ def leiaEnt(txt):  # Lê uma entrada e verifica se ela está dentro do parâmetr
     except:
         print('Ocorreu um erro.')
     else:
-        return resp'''
+        return resp
--- main.py
+++ main.py
@@ -2,5 +2,16 @@ from functions import *
 from time import sleep
 
 while True:
-    menu()
+    interface()
+    user = Menu()
+    '''resp = leiaEnt('Escolha uma opção: ')
+    if resp == 1:
+        pass
+    elif resp == 2:
+        pass
+    elif resp == 3:
+        pass
+    else:
+        break'''
     sleep(1)
+    break
